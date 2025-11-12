1676ccce1bf55cafc02f1675de30bf02c9227c2e
Paolo Cozzi <bunop@libero.it>
2025-11-12 23:33:13 +0100

    :memo: update citation information
    Improves user guidance on citing SMARTER data by:
    
    - Displaying a startup message with citation information.
    - Adding a vignette dedicated to explaining proper citation practices.
    
    Also, suppresses messages in the variants vignette setup chunk.

--- R/zzz.R
+++ R/zzz.R
@@ -2,9 +2,11 @@
   version <- utils::packageVersion(pkgname)
   packageStartupMessage(
     "\nWelcome to smarterapi v", version, "!\n\n",
-    "If you use SMARTER data in your research, please cite:\n",
-    "  [Cozzi et al. (2024). SMARTER-database: a tool to integrate SNP ",
-    "array datasets for sheep and goat breeds. GigaByte. DOI: 10.46471/gigabyte.139]\n",
-    "\nUse citation('smarterapi') for BibTeX entry.\n"
+    "If you use SMARTER data in your research, please cite:\n\n",
+    "  Cozzi et al. (2024). SMARTER-database: a tool to integrate SNP ",
+    "array datasets for sheep and goat breeds. GigaByte. DOI: 10.46471/gigabyte.139\n",
+    "\nUse citation('smarterapi') for a BibTeX entry.\n",
+    "\nFor more information on citing SMARTER data, visit the online vignette at\n",
+    "https://cnr-ibba.github.io/r-smarter-api/articles/citing.html\n"
   )
 }
